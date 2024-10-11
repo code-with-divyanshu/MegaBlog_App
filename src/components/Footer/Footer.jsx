a1aeec39a3c92b7283606db75c0bdd96433fe7eb
code-with-divyanshu <divyanshubartwal67@gmail.com>
2024-10-11 19:20:28 +0530

    Customize Footer Component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,31 +4,34 @@ import Logo from "../Logo";
 
 function Footer() {
   return (
-    <section className="relative overflow-hidden py-10 bg-cyan-100 border border-t-2 border-t-black rounded-b-sm">
+    <section className="relative overflow-hidden pt-20 pb-10 border-t-2 border-t-gray-500 rounded-b-sm">
       <div className="relative z-10 mx-auto max-w-7xl px-4">
         <div className="-m-6 flex flex-wrap">
           <div className="w-full p-6 md:w-1/2 lg:w-5/12">
-            <div className="flex h-full flex-col justify-between">
-              <div className="mb-4 inline-flex items-center">
-                <Logo width="100px" />
+            <div className="flex flex-col justify-between">
+              <div className="mb-4 inline-flex justify-center items-center">
+                <Logo className="w-[80px]" />
               </div>
-              <div>
-                <p className="text-sm text-black">
-                  &copy; Copyright 2024. All Rights Reserved by
-                  @Developer_Divyanshu.
-                </p>
+              <div className="text-gray-300 text-sm">
+                Mega Blog App is a dynamic and user-friendly platform designed
+                for bloggers and content creators. Whether you're a seasoned
+                writer or just starting, Mega Blog App offers an intuitive
+                interface that makes it easy to publish and share your thoughts
+                with the world. Built with the latest web technologies, it
+                ensures a seamless and engaging experience for both creators and
+                readers.
               </div>
             </div>
           </div>
           <div className="w-full p-6 md:w-1/2 lg:w-2/12">
             <div className="h-full">
-              <h3 className="tracking-px mb-9  text-xs font-semibold uppercase text-gray-500">
+              <h3 className="tracking-px mb-9  text-[14px] font-semibold uppercase text-gray-500">
                 Other Profile
               </h3>
               <ul>
                 <li className="mb-4">
                   <Link
-                    className=" text-base font-medium text-gray-900 hover:text-gray-700"
+                    className=" text-base font-medium text-white hover:text-gray-500"
                     to="https://devloper-divyanshu.netlify.app/"
                     target="_blank"
                   >
@@ -37,7 +40,7 @@ function Footer() {
                 </li>
                 <li className="mb-4">
                   <Link
-                    className=" text-base font-medium text-gray-900 hover:text-gray-700"
+                    className=" text-base font-medium text-white hover:text-gray-500"
                     to="https://www.linkedin.com/in/divyanshu-bartwal-b7b058286/"
                     target="_blank"
                   >
@@ -46,7 +49,7 @@ function Footer() {
                 </li>
                 <li className="mb-4">
                   <Link
-                    className=" text-base font-medium text-gray-900 hover:text-gray-700"
+                    className=" text-base font-medium text-white hover:text-gray-500"
                     to="https://github.com/code-with-divyanshu"
                     target="_blank"
                   >
@@ -55,7 +58,7 @@ function Footer() {
                 </li>
                 <li>
                   <Link
-                    className=" text-base font-medium text-gray-900 hover:text-gray-700"
+                    className=" text-base font-medium text-white hover:text-gray-500"
                     to="https://x.com/DivyanshuB21040"
                     target="_blank"
                   >
@@ -67,13 +70,13 @@ function Footer() {
           </div>
           <div className="w-full p-6 md:w-1/2 lg:w-2/12">
             <div className="h-full">
-              <h3 className="tracking-px mb-9  text-xs font-semibold uppercase text-gray-500">
+              <h3 className="tracking-px mb-9  text-[14px] font-semibold uppercase text-gray-500">
                 Support
               </h3>
               <ul>
                 <li className="mb-4">
                   <Link
-                    className=" text-base font-medium text-gray-900 hover:text-gray-700"
+                    className=" text-base font-medium text-white hover:text-gray-500"
                     to="/"
                   >
                     Account
@@ -81,7 +84,7 @@ function Footer() {
                 </li>
                 <li className="mb-4">
                   <Link
-                    className=" text-base font-medium text-gray-900 hover:text-gray-700"
+                    className=" text-base font-medium text-white hover:text-gray-500"
                     to="/"
                   >
                     Help
@@ -89,7 +92,7 @@ function Footer() {
                 </li>
                 <li className="mb-4">
                   <Link
-                    className=" text-base font-medium text-gray-900 hover:text-gray-700"
+                    className=" text-base font-medium text-white hover:text-gray-500"
                     to="/"
                   >
                     Contact Us
@@ -97,7 +100,7 @@ function Footer() {
                 </li>
                 <li>
                   <Link
-                    className=" text-base font-medium text-gray-900 hover:text-gray-700"
+                    className=" text-base font-medium text-white hover:text-gray-500"
                     to="/"
                   >
                     Customer Support
@@ -108,13 +111,13 @@ function Footer() {
           </div>
           <div className="w-full p-6 md:w-1/2 lg:w-3/12">
             <div className="h-full">
-              <h3 className="tracking-px mb-9  text-xs font-semibold uppercase text-gray-500">
+              <h3 className="tracking-px mb-9 text-[14px] font-semibold uppercase text-gray-500">
                 Legals
               </h3>
               <ul>
                 <li className="mb-4">
                   <Link
-                    className=" text-base font-medium text-gray-900 hover:text-gray-700"
+                    className=" text-base font-medium text-white hover:text-gray-500"
                     to="/"
                   >
                     Terms &amp; Conditions
@@ -122,7 +125,7 @@ function Footer() {
                 </li>
                 <li className="mb-4">
                   <Link
-                    className=" text-base font-medium text-gray-900 hover:text-gray-700"
+                    className=" text-base font-medium text-white hover:text-gray-500"
                     to="/"
                   >
                     Privacy Policy
@@ -130,7 +133,7 @@ function Footer() {
                 </li>
                 <li>
                   <Link
-                    className=" text-base font-medium text-gray-900 hover:text-gray-700"
+                    className=" text-base font-medium text-white hover:text-gray-500"
                     to="/"
                   >
                     Licensing
@@ -139,6 +142,11 @@ function Footer() {
               </ul>
             </div>
           </div>
+        </div>
+        <div className="mt-10">
+          <p className="text-gray-400  text-center">
+            &copy; Copyright 2024. All Rights Reserved by @Developer_Divyanshu.
+          </p>
         </div>
       </div>
     </section>
