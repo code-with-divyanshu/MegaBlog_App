21c0943bda90c0333dd321a871cfc04e71ab9dbb
code-with-divyanshu <divyanshubartwal67@gmail.com>
2024-10-11 19:57:38 +0530

    Customize Logo Component

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-function Logo() {
+function Logo({ className }) {
   return (
     <div>
       <img
         src="https://cdn.logojoy.com/wp-content/uploads/2018/05/30164225/572.png"
         alt=""
-        className="w-[50px] rounded-xl"
+        className={`w-[50px] rounded-xl ${className} || "" `}
       />
     </div>
   );
