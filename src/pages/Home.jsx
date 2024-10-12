f2efc88bd8c896972d3f3abc564bfbc55702e2b3
code-with-divyanshu <divyanshubartwal67@gmail.com>
2024-10-12 20:01:23 +0530

    Customize Home Component and Page Component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import appwriteService from "../appwrite/config";
-import { Container, PostCard } from "../components";
+import { Button, Container, PostCard } from "../components";
+import { blog } from "../assets";
 
 function Home() {
   const [posts, setPosts] = useState([]);
@@ -22,11 +23,51 @@ function Home() {
     return (
       <div className="w-full py-8 mt-4 text-center">
         <Container>
-          <div className="flex flex-wrap">
-            <div className="p-2 w-full">
-              <h1 className="text-2xl font-bold hover:text-sky-500">
-                Login to read posts
+          <div className="flex flex-col lg:flex-row justify-center items-center py-10 gap-10">
+            <div className="w-full">
+              <h1 className="uppercase text-4xl my-8 lg:text-5xl font-bold bg-gradient-to-r from-blue-600 via-purple-500 to-indigo-400 inline-block text-transparent bg-clip-text hover:text-sky-500">
+                Mega Blog App
               </h1>
+              <p className="text-gray-300">
+                Mega Blog App is a modern blogging platform crafted to empower
+                writers, creators, and developers. With its sleek design,
+                responsive layout, and customizable features, it offers
+                everything you need to manage and showcase your content
+                effortlessly. Enjoy rich text formatting, media embedding, and
+                an organized content management system—all in one place. The app
+                supports a variety of blog categories, making it versatile for
+                personal blogs, tech blogs, lifestyle stories, and more. Join
+                the Mega Blog App community and take your online presence to the
+                next level!
+              </p>
+            </div>
+            <div className="h-full w-full rounded-md bg-gradient-to-r from-blue-500 via-red-500 to-purple-500 p-1">
+              <div className="w-full h-full bg-gray-800 rounded-xl p-4">
+                <img src={blog} alt="Blog Image" className="rounded-xl" />
+              </div>
+            </div>
+          </div>
+          <div className="my-10">
+            <h1 className="text-2xl uppercase font-bold bg-gradient-to-r from-blue-600 via-purple-500 to-indigo-400 inline-block text-transparent bg-clip-text hover:text-sky-500">
+              Create Account or Login to Read and Write Blog
+            </h1>
+          </div>
+          <div className="flex flex-col lg:flex-row justify-center items-center gap-10">
+            <div>
+              <a
+                href="/signup"
+                className="w-full capitalize font-semibold p-4 text-white hover:text-amber-900 rounded-md bg-gradient-to-r from-blue-600 via-teal-500 to-indigo-400"
+              >
+                Create New Account
+              </a>
+            </div>
+            <div>
+              <a
+                href="/login"
+                className="w-full capitalize font-semibold p-4 text-white hover:text-amber-900 rounded-md bg-gradient-to-r from-blue-600 via-teal-500 to-indigo-400"
+              >
+                Login to existing account
+              </a>
             </div>
           </div>
         </Container>
