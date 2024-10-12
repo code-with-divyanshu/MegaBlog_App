e695ad9e0bc1f10dc4346e013e30f0ebbb96182b
code-with-divyanshu <divyanshubartwal67@gmail.com>
2024-10-12 18:48:29 +0530

    Customize Input Component

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -8,13 +8,16 @@ const Input = React.forwardRef(function Input(
   return (
     <div className="w-full">
       {label && (
-        <label className="inline-block mb-1 pl-1" htmlFor={Id}>
+        <label
+          className="inline-block mb-1 pl-1 text-xl lg:text-2xl"
+          htmlFor={Id}
+        >
           {label}
         </label>
       )}
       <input
         type={type}
-        className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}
+        className={`px-3 py-2 lg:py-3 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}
         ref={ref}
         {...props}
         id={Id}
