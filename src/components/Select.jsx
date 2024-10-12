b1db8a954ae7f901c628e5a71c503d9f88fd99be
code-with-divyanshu <divyanshubartwal67@gmail.com>
2024-10-12 18:48:41 +0530

    Customize Select Component

--- src/components/Select.jsx
+++ src/components/Select.jsx
@@ -9,7 +9,7 @@ function Select({ options, label, className = "", ...props }, ref) {
         {...props}
         id={id}
         ref={ref}
-        className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}
+        className={`px-3 py-3 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}
       >
         {options?.map((option) => (
           <option key={option} value={option}>
