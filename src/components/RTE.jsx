ab5b675c0ea31a858a5b9fa7d1f699529c5e90c7
code-with-divyanshu <divyanshubartwal67@gmail.com>
2024-10-12 18:49:01 +0530

    Customize RTE Editor Component

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -5,7 +5,11 @@ import { Controller } from "react-hook-form";
 export default function RTE({ name, control, label, defaultValue = "" }) {
   return (
     <div className="w-full">
-      {label && <label className="inline-block mb-1 pl-1">{label}</label>}
+      {label && (
+        <label className="inline-block text-xl lg:text-2xl mb-1 pl-1">
+          {label}
+        </label>
+      )}
       <Controller
         name={name || "content"}
         control={control}
