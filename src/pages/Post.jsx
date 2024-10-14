40540cf4db720283a6534d197228a0338b7cae52
code-with-divyanshu <divyanshubartwal67@gmail.com>
2024-10-14 13:13:19 +0530

    Customize Post Component

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -35,30 +35,42 @@ export default function Post() {
   return post ? (
     <div className="py-8">
       <Container>
-        <div className="w-full flex justify-center mb-4 relative border rounded-xl p-2">
-          <img
-            src={appwriteService.getFilePreview(post.featuredImage)}
-            alt={post.title}
-            className="rounded-xl lg:h-[500px] w-[90%]"
-          />
-
-          {isAuthor && (
-            <div className="absolute right-6 top-6">
-              <Link to={`/edit-post/${post.$id}`}>
-                <Button bgColor="bg-green-500" className="mr-3">
-                  Edit
-                </Button>
-              </Link>
-              <Button bgColor="bg-red-500" onClick={deletePost}>
-                Delete
-              </Button>
+        <div className="flex flex-col-reverse lg:flex-row gap-16">
+          <div className="w-full my-auto">
+            <div className="w-full mb-6 text-white">
+              <h1 className="text-2xl lg:text-3xl font-bold uppercase bg-gradient-to-r from-blue-600 via-purple-500 to-indigo-400 inline-block text-transparent bg-clip-text">
+                {post.title}
+              </h1>
             </div>
-          )}
+            <div className="browser-css text-white text-center lg:text-[18px]">
+              {parse(post.content)}
+            </div>
+          </div>
+          <div className="relative mx-auto flex w-full items-center justify-center">
+            <div className="h-full w-full rounded-md bg-gradient-to-r from-blue-500 via-red-500 to-purple-500 p-1">
+              <div className="w-full h-full bg-gray-800 rounded-xl p-4">
+                <img
+                  src={appwriteService.getFilePreview(post.featuredImage)}
+                  alt={post.title}
+                  className="rounded-xl lg:h-[400px] w-full"
+                />
+
+                {isAuthor && (
+                  <div className="absolute right-6 top-6">
+                    <Link to={`/edit-post/${post.$id}`}>
+                      <Button bgColor="bg-green-500" className="mr-3">
+                        Edit
+                      </Button>
+                    </Link>
+                    <Button bgColor="bg-red-500" onClick={deletePost}>
+                      Delete
+                    </Button>
+                  </div>
+                )}
+              </div>
+            </div>
+          </div>
         </div>
-        <div className="w-full mb-6">
-          <h1 className="text-2xl font-bold">{post.title}</h1>
-        </div>
-        <div className="browser-css text-center">{parse(post.content)}</div>
       </Container>
     </div>
   ) : null;
