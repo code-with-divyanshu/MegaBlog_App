ebf9bcecce9679c2011a7d69fa3d48019569279f
code-with-divyanshu <divyanshubartwal67@gmail.com>
2024-10-15 11:11:00 +0530

    Customize Edit Post Page & Components

--- src/pages/EditPost.jsx
+++ src/pages/EditPost.jsx
@@ -26,8 +26,11 @@ function EditPost() {
   }, [slug, navigate]);
 
   return post ? (
-    <div className="py-8">
+    <div className="py-8 text-white">
       <Container>
+        <h1 className="text-2xl lg:text-3xl mb-10 uppercase font-bold bg-gradient-to-r from-blue-600 via-purple-500 to-indigo-400 inline-block text-transparent bg-clip-text hover:text-sky-500">
+          Edit Post
+        </h1>
         <PostForm post={post} />
       </Container>
     </div>
