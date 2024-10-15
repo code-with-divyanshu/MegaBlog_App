11b95ed76150dae6d955c0c281d7ec891f28c74f
code-with-divyanshu <divyanshubartwal67@gmail.com>
2024-10-15 11:10:28 +0530

    Customize Add Post Page & Components

--- src/pages/AddPost.jsx
+++ src/pages/AddPost.jsx
@@ -3,8 +3,11 @@ import { Container, PostForm } from "../components";
 
 function AddPost() {
   return (
-    <div className="py-8">
+    <div className="py-8 text-white">
       <Container>
+        <h1 className="text-2xl lg:text-3xl mb-10 uppercase font-bold bg-gradient-to-r from-blue-600 via-purple-500 to-indigo-400 inline-block text-transparent bg-clip-text hover:text-sky-500">
+          Add Posts
+        </h1>
         <PostForm />
       </Container>
     </div>
